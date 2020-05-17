2c4aaaa929daf82b4b0fb8babf008677fb6449ec
m4ik7i <m4ik7i@gmail.com>
2020-05-18 02:32:38 +0900

    chore: modify deps

--- deps.js
+++ deps.js
@@ -1,4 +1,2 @@
-import React from 'https://dev.jspm.io/react';
-import ReactDOM from 'https://dev.jspm.io/react-dom';
-
-export { React, ReactDOM };
+export { default as React } from 'https://dev.jspm.io/react';
+export { default as ReactDOM } from 'https://dev.jspm.io/react-dom';
