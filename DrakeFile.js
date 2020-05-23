ac4837a451d01e73996831dc828677a9b57cc95d
m4ik7i <m4ik7i@gmail.com>
2020-05-24 03:31:34 +0900

    refactor: modify DrakeFile

--- DrakeFile.js
+++ DrakeFile.js
@@ -3,6 +3,9 @@ import { run, sh, task } from 'https://deno.land/x/drake/mod.ts';
 const DEV_DIR = 'dev';
 const BUILD_DIR = 'build';
 
+const babel = './node_modules/.bin/babel';
+const serve = 'deno run --allow-net --allow-read scripts/serve.js';
+
 task('clean:dev', [], async () => {
   await sh(`rm -rf ${DEV_DIR}`);
 });
@@ -14,35 +17,19 @@ task('clean:build', [], async () => {
 task('dev', [], async () => {
   await run('clean:dev');
   await sh(`cp -pr public ${DEV_DIR}`);
-  await run('babel');
-  await run('serve:dev');
-});
-
-task('babel', [], async () => {
-  await sh(`./node_modules/.bin/babel src --out-dir ${DEV_DIR}/js`);
+  await sh(`${babel} src --out-dir ${DEV_DIR}/js`);
+  await sh(`${serve} ${DEV_DIR} 3000`);
 });
 
 task('build', [], async () => {
   await run('clean:build');
   await sh(`cp -pr public ${BUILD_DIR}`);
-  await run('bundle');
-});
-
-task('bundle', [], async () => {
   await sh(`mkdir -p ${BUILD_DIR}/js`);
   await sh(`deno bundle src/main.js ${BUILD_DIR}/js/main.js`);
 });
 
-const serve = (dir = '.', port = 5000) => {
-  return sh(`deno run --allow-net --allow-read scripts/serve.js ${dir} ${port}`);
-};
-
-task('serve:dev', [], async () => {
-  await serve('dev', 3000);
-});
-
 task('serve', [], async () => {
-  await serve('build', 5000);
+  await sh(`${serve} ${BUILD_DIR}`);
 });
 
 run();
