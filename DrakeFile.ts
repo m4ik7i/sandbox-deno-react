43cdb64b733ad0fc805bc38b6ae3815c8dc342f8
m4ik7i <m4ik7i@gmail.com>
2020-05-26 00:22:41 +0900

    chore: add .vscode

---
+++ DrakeFile.ts
@@ -0,0 +1,35 @@
+import { run, sh, task } from "https://deno.land/x/drake/mod.ts";
+
+const DEV_DIR = "dev";
+const BUILD_DIR = "build";
+
+const babel = "./node_modules/.bin/babel";
+const serve = "deno run --allow-net --allow-read scripts/serve.ts";
+
+task("clean:dev", [], async () => {
+  await sh(`rm -rf ${DEV_DIR}`);
+});
+
+task("clean:build", [], async () => {
+  await sh(`rm -rf ${BUILD_DIR}`);
+});
+
+task("dev", [], async () => {
+  await run("clean:dev");
+  await sh(`cp -pr public ${DEV_DIR}`);
+  await sh(`${babel} src --out-dir ${DEV_DIR}/js`);
+  await sh(`${serve} ${DEV_DIR} 3000`);
+});
+
+task("build", [], async () => {
+  await run("clean:build");
+  await sh(`cp -pr public ${BUILD_DIR}`);
+  await sh(`mkdir -p ${BUILD_DIR}/js`);
+  await sh(`deno bundle src/main.js ${BUILD_DIR}/js/main.js`);
+});
+
+task("serve", [], async () => {
+  await sh(`${serve} ${BUILD_DIR}`);
+});
+
+run();
--- scripts/serve.ts
+++ scripts/serve.ts
@@ -1,14 +1,14 @@
-import { Application, send } from 'https://deno.land/x/oak/mod.ts';
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
 
-const DIR = Deno.args[0] || '.';
-const PORT = Number(Deno.args[1] || '5000');
+const DIR = Deno.args[0] || ".";
+const PORT = Number(Deno.args[1] || "5000");
 
 const app = new Application();
 
 app.use(async (ctx) => {
   await send(ctx, ctx.request.url.pathname, {
     root: `${Deno.cwd()}/${DIR}`,
-    index: 'index.html',
+    index: "index.html",
   });
 });
 
